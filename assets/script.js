aa8f85280cc6191babd6bf1313030c963459434b
Tro <noe.monsivais@gmail.com>
2020-03-01 10:39:28 -0600

    Changed background color on body, got timer to count down and stop at zero.

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,55 @@
 var questionBank = [
     {
-    question: 'first question',
-    choices: [ 'a','b','c','d'],
-    answer: 'b'
-},
-{
-    question: ' asdfasdf',
-    choices: [ 'a','b','c','d'],
-    answer: 'b'
-},
-{
-    question: ' asdfasdf',
-    choices: [ 'a','b','c','d'],
-    answer: 'b'
-}] ;
+        question: 'This is my first question?',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'b'
+    },
+    {
+        question: 'This is my second question?',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'b'
+    },
+    {
+        question: 'This is my third question',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'b'
+    }];
 //create a timer that counts down.
- 
+var timer = document.querySelector(".timer");
+//User Question appear below to make question and button choices appear.//
+var secondsLeft = 5;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        // .textContent updates front end element between > <
+        timer.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            // stoping the interval / timer
+            clearInterval(timerInterval);
+            // execute the sendMessage function
+            sendMessage();
+        }
+
+    }, 1000);
+}
+
+function sendMessage() {
+    timer.textContent = " ";
+}
+
+setTime();
 //Link the timer to activate when the START button is clicked.
 
+
 //Prepare the first question with multiple choices and display it on page.
-function showQuestion (){
-console.log('ShowQuestion executed')
-// grabbed front end element
-var questionBankDisplay = document.querySelector ('.question');
-// updated that element with the first questions
-questionBankDisplay.textContent = questionBank[0].question
+function showQuestion() {
+    console.log('ShowQuestion executed')
+    // grabbed front end element
+    var questionBankDisplay = document.querySelector('.question');
+    // updated that element with the first questions
+    questionBankDisplay.textContent = questionBank[0].question
 
 
 }
